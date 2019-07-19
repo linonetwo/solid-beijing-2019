e2bfaec0070ad022122846bdf7780c3ea4b38967
linonetwo <linonetwo012@gmail.com>
2019-07-19 23:03:06 +0800

    initial

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+  fonts: {
+    body: 'Roboto, sans-serif',
+    monospace: '"Roboto Mono", monospace',
+  },
+  colors: {
+    text: '#7C4DFF',
+    background: 'white',
+    primary: '#083575',
+  },
+};
